39501bc8f40c08376592168d2569f6bc7daa824e
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-25 11:38:08 +0100

    Commit crud

--- Brief_2/crudintro.php
+++ Brief_2/crudintro.php
@@ -1,4 +1,32 @@
 <?php
+
+// Information de connexion a la base de données
+$host = "localhost";
+$dbname = "bibliotheque";
+$user = "root";
+$pass="";
+
+try {
+    // Création d'une instance PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    // Configuration de PDO en cas d'exception
+    $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+}    catch (PDOException $e) {
+    // S'il y a une erreur de connexion
+    die ("Erreur de connexion : " . $e -> getMessage());
+}
+
+// Préparation de la requête
+$query = "SELECT * FROM auteur";
+
+// Execution de la requête
+$stmt  = $pdo -> query($query);
+
+// Récupération des données (tableau associatif)
+$auteurs = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+print_r($auteurs);
+
 ?>
 <!doctype html>
 <html lang="en">
