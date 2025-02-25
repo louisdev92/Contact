7768aa17a860f0f155b2bf65310828edd37fd809
louis.hameau <louis.hameau@campus-centre.fr>
2025-02-25 11:23:46 +0100

    Commit crud

---
+++ Brief_2/crudintro.php
@@ -0,0 +1,26 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+    <title>Liste des livres</title>
+</head>
+<body>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Date de naissance</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+</body>
+</html>
